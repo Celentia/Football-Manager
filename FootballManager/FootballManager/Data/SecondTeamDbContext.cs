f2eb7bd7f6c899ab77e65e6fb228ab099de5a9ae
Marharyta Poliakovska <marharyta.poliakovska@ihsmarkit.com>
2020-05-30 14:21:25 +0300

    feat: create graphic interface and implement CRUD operations for a football manager

---
+++ FootballManager/FootballManager/Data/SecondTeamDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballManager.Data
+{
+    public class SecondTeamDbContext : DbContext
+    {
+        public SecondTeamDbContext(DbContextOptions<SecondTeamDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<SecondTeamPlayer> Players { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SecondTeamPlayer>().HasData(GetPlayers());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private SecondTeamPlayer[] GetPlayers()
+        {
+            return new SecondTeamPlayer[]
+            {
+            new SecondTeamPlayer { Id = 1, FirstName = "Jack", LastName = "Black", Position = "Goalkeeper", Birthday = DateTime.ParseExact("2019-09-01", "yyyy-MM-dd", CultureInfo.InvariantCulture)},
+            new SecondTeamPlayer { Id = 2, FirstName = "John", LastName = "Smith", Position = "Striker", Birthday = DateTime.ParseExact("2019-09-02", "yyyy-MM-dd", CultureInfo.InvariantCulture)},
+            new SecondTeamPlayer { Id = 3, FirstName = "Derek", LastName = "Poole", Position = "Full-backs", Birthday = DateTime.ParseExact("2019-09-03", "yyyy-MM-dd", CultureInfo.InvariantCulture)},
+            new SecondTeamPlayer { Id = 4, FirstName = "Chris", LastName = "Fan", Position = "Full-backs", Birthday = DateTime.ParseExact("2019-09-04", "yyyy-MM-dd", CultureInfo.InvariantCulture)},
+            };
+        }
+    }
+}
---
+++ FootballManager/FootballManager/Matches.xaml.cs
@@ -0,0 +1,56 @@
+﻿using FootballManager.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FootballManager
+{
+    /// <summary>
+    /// Interaction logic for Matches.xaml
+    /// </summary>
+    public partial class Matches : Window
+    {
+        private readonly ServiceProvider serviceProvider;
+ 
+        public Matches() {
+            ServiceCollection services = new ServiceCollection();
+            services.AddDbContext<FirstTeamDbContext>(options =>
+            {
+                options.UseSqlite("Data Source = FirstTeam.db");
+            });
+            services.AddDbContext<SecondTeamDbContext>(options =>
+            {
+                options.UseSqlite("Data Source = SecondTeam.db");
+            });
+            services.AddSingleton<FirstTeamWindow>();
+            services.AddSingleton<SecondTeamWindow>();
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FirstTeamWindow window1 = new FirstTeamWindow();
+            window1 = serviceProvider.GetService<FirstTeamWindow>();
+            window1.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SecondTeamWindow window2 = new SecondTeamWindow();
+            window2 = serviceProvider.GetService<SecondTeamWindow>();
+            window2.Show();
+        }
+    }
+}
